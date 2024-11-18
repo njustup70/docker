679be3f12ac560f88ea2dea6a7b2fb627bc651b8
Yc-Dlan <1940275781@qq.com>
2024-11-18 13:55:46 +0000

    feat（ros2）针对urdf文件对tf功能包做了部分修改

--- ros2/ros2_ws/src/my_tfTree/setup.py
+++ ros2/ros2_ws/src/my_tfTree/setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
-
+import glob
 package_name = 'my_tfTree'
 
 setup(
@@ -10,7 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch'),
+        ('share/' + package_name + '/launch'), glob.glob('urdf/*')
+        ('share/' + package_name + '/urdf'), glob.glob('urdf/*')
     ],
     install_requires=['setuptools'],
     zip_safe=True,
