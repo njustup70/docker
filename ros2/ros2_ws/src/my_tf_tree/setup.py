443079343fcac4207b79c85678ac8d08c13c06c7
Yc-Dlan <1940275781@qq.com>
2024-11-19 14:18:29 +0000

    fix（ros2）修正了setup.py文件的urdf配置问题

--- ros2/ros2_ws/src/my_tf_tree/setup.py
+++ ros2/ros2_ws/src/my_tf_tree/setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
 import glob
-package_name = 'my_tf_tree'
+
+package_name = 'my_tf_tree'  # 确保包名符合规范
 
 setup(
     name=package_name,
@@ -10,8 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch'), glob.glob('urdf/*')
-        ('share/' + package_name + '/urdf'), glob.glob('urdf/*')
+        ('share/' + package_name + '/launch', glob.glob('launch/*.launch.py')),  # 修正launch文件的路径
+        ('share/' + package_name + '/urdf', glob.glob('urdf/*')),  # 修正urdf文件的路径
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,10 +23,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'broadcaster_node = my_tfTree.broadcaster_node:main',
-            'listener_node = my_tfTree.listener_node:main',
-            'radar_node = my_tfTree.radar_node:main',
-            'depth_camera_node = my_tfTree.depth_camera_node:main',
+            'broadcaster_node = my_tf_tree.broadcaster_node:main',  # 修正包名和节点名
+            'listener_node = my_tf_tree.listener_node:main',  # 修正包名和节点名
+            'radar_node = my_tf_tree.radar_node:main',  # 修正包名和节点名
+            'depth_camera_node = my_tf_tree.depth_camera_node:main',  # 修正包名和节点名
         ],
     },
 )
