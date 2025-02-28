2fa68a81157ad0acc7a0db4055d6592a32dd25a4
Elaina <1463967532@qq.com>
2025-02-28 13:36:24 +0000

    fix(ros2-module)更改了rosbag脚本的名字,并且安装到install目录

--- ros2-modules/src/rc_bringup/launch/rosbag_bridge_rviz.launch.py
+++ ros2-modules/src/rc_bringup/launch/rosbag_bridge_rviz.launch.py
@@ -9,13 +9,18 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.descriptions import ComposableNode
 from launch_ros.actions import ComposableNodeContainer, Node
 def generate_launch_description():
+    local_path=os.path.join(get_package_share_directory('rc_bringup'))
     ld=LaunchDescription()
+    ld.add_action(DeclareLaunchArgument('use_rosbag_record', default_value='true', description='Record rosbag if use is True'))
+    # ld.add_action(DeclareLaunchArgument('ros', default_value='5', description='Max number of rosbag files'))
     foxglove_node=ComposableNode(
         package='foxglove_bridge',
         plugin='foxglove_bridge::FoxgloveBridge',
@@ -28,5 +33,28 @@ def generate_launch_description():
         cmd=["bash","-c","~/docker/ros2-modules/packages/ros-bridge/ros_bridge_run.sh"],
         output='screen',
     )
+    ros_bag_bash_path=os.path.join(local_path,'scripts/rosbag_record.py')
+    ros_bag_exe=ExecuteProcess(
+        cmd=["bash","-c","python3 {}".format(ros_bag_bash_path)],
+        output='screen',
+    )
+    compose_container=ComposableNodeContainer(
+        namespace='',
+        name='start_container',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[foxglove_node],
+        output='screen',
+        emulate_tty=True,
+    )
+    
+    # rosbag_record=LaunchConfiguration('rosbag_record')
+    # if_condition = IfCondition(LaunchConfiguration('rosbag_record'))
+    # print("rosbag_record:",if_condition )
+    # if(rosbag_record=='True'):
+    #     print("rosbag record")
+    #     ld.add_action(rosbag_record)
     ld.add_action(ros_bridge_exe)
+    ld.add_action(ros_bag_exe)
+    ld.add_action(compose_container)
     return ld
