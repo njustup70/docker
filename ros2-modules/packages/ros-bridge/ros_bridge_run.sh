5b19c68a7e5c64a9e6ccafe637b93dfd5909fbdc
Elaina <1463967532@qq.com>
2025-02-28 11:45:39 +0000

    fix(ros2-module)修复自动运行桥接的脚本路径是基于运行目录而不是脚本目录问题

--- ros2-modules/packages/ros-bridge/ros_bridge_run.sh
+++ ros2-modules/packages/ros-bridge/ros_bridge_run.sh
@@ -2,17 +2,19 @@
 
 # 使脚本支持 Ctrl+C 快速退出
 trap 'echo "Exiting..."; kill $(jobs -p)' SIGINT
+# 获取当前脚本所在目录
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # 选择架构并配置环境
 source ~/.bashrc
 if [ `uname -m` == "x86_64" ]; then
     # x86
     echo "x86"
-    source x86/setup.bash
+    source $SCRIPT_DIR/x86/setup.bash
 else
     # arm
     echo "arm"
-    source arm/setup.bash
+    source $SCRIPT_DIR/arm/setup.bash
 fi
 
 # 启动 ros2 动态桥接，并将其放入后台
