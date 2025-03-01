52fceaeac3ad4ecbbb23e005dd027f5b557512a2
Elaina <1463967532@qq.com>
2025-03-01 12:54:29 +0000

    feat(ros2-modules)添加了一个播放已经有的包并启动辅助节点的launch

---
+++ ros2-modules/src/rc_bringup/launch/rosbag_with_utils.launch.py
@@ -0,0 +1,54 @@
+'''
+启动驱动节点和工具节点
+'''
+import os
+import sys
+import glob
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess,IncludeLaunchDescription,DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch.substitutions import Command, PathJoinSubstitution, FindExecutable
+
+def generate_launch_description():
+    ld=LaunchDescription()
+    ld.add_action(DeclareLaunchArgument('use_rosbag_record', default_value='false', description='Record rosbag if use is True'))
+    ld.add_action(DeclareLaunchArgument('use_tf_publish',default_value='false',description='Publish tf tree if use is True'))
+    get_package_share_directory('rc_bringup')
+    #启动rosbag
+    rosbag_root_path='~/docker/ros2-modules/bag_play'
+    #查找root_path中的第一个文件夹中的db播放
+    folders = [d for d in os.listdir(rosbag_root_path) if os.path.isdir(os.path.join(rosbag_root_path, d))]
+    if folders:
+        first_folder = folders[0]  # 取第一个文件夹
+        rosbag_path = os.path.join(rosbag_root_path, first_folder)
+
+        # 查找 .db3 文件
+        db3_files = glob.glob(os.path.join(rosbag_path, "*.db3"))
+        if db3_files:
+            # 播放第一个 .db3 文件
+            os.system(f"ros2 bag play {db3_files[0]}")
+        else:
+            print("没有找到 .db3 文件")
+    else:
+        print("没有找到文件夹")
+    ros_bag_exe=ExecuteProcess(
+        cmd=["bash","-c","ros2 bag play --loop {}".format(rosbag_path)],
+        output='screen'
+    )
+
+    #启动utils
+    utils_launch=IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('rc_bringup'),'launch','utils_bringup.launch.py')
+        ),
+        
+    )
+    ld.add_action(ros_bag_exe)
+    ld.add_action(utils_launch)
+    return ld
+     
