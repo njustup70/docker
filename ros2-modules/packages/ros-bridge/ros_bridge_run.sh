1c6aa8a0a1247b3966b131df804df8eb0c487495
Elaina <1463967532@qq.com>
2025-03-04 14:17:33 +0000

    fix(ros2-modules)增加检查11311端口占用,防止非roscore情况下内存泄漏问题

--- ros2-modules/packages/ros-bridge/ros_bridge_run.sh
+++ ros2-modules/packages/ros-bridge/ros_bridge_run.sh
@@ -5,16 +5,34 @@ trap 'echo "Exiting..."; kill $(jobs -p)' SIGINT
 # 获取当前脚本所在目录
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+
+# 检查端口 11311 是否被占用
+check_port() {
+    local port=11311
+    local result=$(sudo netstat -tulnp | grep ":$port")
+
+    if [[ -n "$result" ]]; then
+        if echo "$result" | grep -q "python3"; then
+            echo -e "\033[1;32m端口 $port 被 python3 进程占用，继续执行。\033[0m"
+        else
+            echo -e "\033[1;31m错误: 端口 $port 被非 python3 进程占用！\033[0m"
+            echo -e "\033[1;31m请检查后再运行此脚本。\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+check_port
 # 选择架构并配置环境
 source ~/.bashrc
 if [ `uname -m` == "x86_64" ]; then
     # x86
     echo "x86"
-    source $SCRIPT_DIR/x86/setup.bash
+    source $SCRIPT_DIR/x86/local_setup.bash
 else
     # arm
     echo "arm"
-    source $SCRIPT_DIR/arm/setup.bash
+    source $SCRIPT_DIR/arm/local_setup.bash
 fi
 
 # 启动 ros2 动态桥接，并将其放入后台
