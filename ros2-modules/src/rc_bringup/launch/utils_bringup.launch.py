44ec5b6a85bc12789549d0bbf056b551b5f741c9
Hu Chuyi <923110800623@njust.edu.cn>
2025-04-22 16:02:17 +0000

    feat(tf)改正了静态tf参数未生效的问题

--- ros2-modules/src/rc_bringup/launch/utils_bringup.launch.py
+++ ros2-modules/src/rc_bringup/launch/utils_bringup.launch.py
@@ -77,22 +77,23 @@ def generate_launch_description():
         parameters=[{
         'child_frame_id': 'base_link',
         'frame_id': 'body',
-        'translation': [0.0, 0.0, 0.0],
+        'translation': {'x': 0.0, 'y': 0.0, 'z': 0.0}, 
         'rotation': [0.0, 0.0, 0.0]
     }],
     )
-    fast_lio_tf_node2=ComposableNode(
+    fast_lio_tf_node2 = ComposableNode(
         condition=IfCondition(LaunchConfiguration('use_fast_lio_tf')),
         package='tf2_ros',
         plugin='tf2_ros::StaticTransformBroadcasterNode',
-        name='tf_broadcaster',
+        name='tf_broadcaster1',
         parameters=[{
-        'child_frame_id': 'camera_init',
-        'frame_id': 'map',
-        'translation': [0.0, 0.0, 0.0],
-        'rotation': [0.0, 0.0, 0.0]
-    }],
+            'child_frame_id': 'camera_init',  # 旋转后坐标系
+            'frame_id': 'map',  # 参考坐标系
+            'translation': {'x': 0.0, 'y': 0.0, 'z': 0.0}, 
+            'rotation': {'x':0.0, 'y':0.0, 'z':0.7071, 'w':0.7071}  # 四元数表示的 90 度旋转（绕 Z 轴）
+        }],
     )
+
     compose_container=ComposableNodeContainer(
         namespace='',
         name='start_container',
