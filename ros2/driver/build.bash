1abbba94199a68a9edff05fc211d24bf87925699
nagisa <2964793117@qq.com>
2025-12-02 20:37:07 +0800

    修改镜像对应仓库

--- ros2/driver/build.bash
+++ ros2/driver/build.bash
@@ -12,7 +12,7 @@ echo "父目录: $PARENT_DIR"
 TAG="ros2_driver"
 
 # 从外部传入的 IMAGE_REPO（格式：ghcr.io/user/repo 或 docker.io/user/repo）
-IMAGE_REPO=${IMAGE_REPO:-elainasuki/rc2026}
+IMAGE_REPO="elainasuki/rc2026"
 
 # 组合完整镜像名
 IMAGE="$IMAGE_REPO:$TAG"
